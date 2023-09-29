914c1ce8f50eb8ed7adbde85e9488e6c52de27aa
dmitrmedv <dmitrmedv@gmail.com>
2023-09-29 08:28:37 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import LayOut from 'LayOut';
+import fetchMovies, { getMoviesList } from 'api/api';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies/Movies';
 import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
+  {
+    getMoviesList().then(console.log);
+  }
   return (
     <Routes>
       <Route path="/" element={<LayOut />}>
---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'cdfc08b3296f0164149119b33e9bcc21';
+
+const instance = axios.create({
+  baseURL: `https://api.themoviedb.org/3`,
+});
+
+export async function getMoviesList() {
+  const { data } = await instance.get(`/trending/all/week?api_key=${KEY}`);
+  return data;
+}
