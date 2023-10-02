8dee159250f5ad850ad8eb210a6a84fbaa3f8cbb
dmitrmedv <dmitrmedv@gmail.com>
2023-10-02 23:23:36 +0300

    add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import css from './Header.module.css';
 
 const Header = () => {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="movies">Movies</NavLink>
+    <div className={css.header}>
+      <NavLink to="/" className={css.link}>
+        Home
+      </NavLink>
+      <NavLink to="movies" className={css.link}>
+        Movies
+      </NavLink>
     </div>
   );
 };
